71924c853b5cfebbd8a9f0ce3a17754204ac386d
Souhail <souhail.ait_lahcen@edu.devinci.fr>
2022-01-02 17:47:28 +0100

    Final Version

--- CryptoProject.py
+++ CryptoProject.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
-import os
 
-#from Crypto.Cipher import AES
-#from Crypto import Random
+import os
 
 #Bcrypt libarie useful for the hashing
 import bcrypt
@@ -12,14 +10,13 @@ import tink
 from tink import daead, cleartext_keyset_handle, KeysetHandle
 
 
-
+#region tink and how to use it
 
 daead.register()
 database = 'database.txt'
 
 #Reader of the .json, necessary to read and write (if we don't this file) the secret_key with tink
 keysetFilename = "my_keyset.json"
-#secret_key = tink.new_keyset_handle(daead.deterministic_aead_key_templates.AES256_SIV)
 
 #if the file exist we read the file
 if os.path.isfile(keysetFilename):
@@ -37,17 +34,13 @@ else:
    secret_key = cleartext_keyset_handle.read(tink.JsonKeysetReader(lecture.read()))
    lecture.close()
 
-#secret_key = read_keyset("my_keyset.json")
 
 daead_primitive = secret_key.primitive(daead.DeterministicAead)
+#endregion
 
 ### Associated
 associated_data = b"Souhail"
 ###
-
-
-
-
 
 #region Hash Password
 def hash_password(pwd:str,salt=None):
@@ -56,10 +49,6 @@ def hash_password(pwd:str,salt=None):
   else:
     salt=salt
   hash=bcrypt.hashpw(pwd.encode(),salt)
-  if bcrypt.checkpw(pwd.encode(),hash):
-    print("good format !")
-  else:
-    print("bad format, sorry")
   return hash,salt
 #endregion
 
@@ -85,33 +74,25 @@ def save_to_database(user, pwd):
   
 #endregion
 
-
+#region check
 def check_password(user, pwd):
   # read from database
   with open(database, 'r') as f:
-    error=0
+    error=1
     test=False
+    print("Welcome user :",user," we will verify if you are in our database...")
     for line in f.readlines():
       userdatabase,encrypteddatabase,salt= line.split(',')
-    print("Welcome user :",user," we will verify if you are on our database...")
-    hash,salt2=hash_password(pwd,bytes.fromhex(salt))
-    password_of_the_current_user=encryption_machine(hash)
-    # and check for authentication
-    encrypteddatabase=bytes.fromhex(encrypteddatabase)
-    if (user == userdatabase) and (encrypteddatabase == password_of_the_current_user):
-      test=True
-      error=0
-      return test,error 
-    elif (user == userdatabase) and (encrypteddatabase != password_of_the_current_user):
-      test=False
-      error=1
-    elif (user != userdatabase) and (encrypteddatabase == password_of_the_current_user):
-      test=False
-      error=2
-    else:
-      test=False
-      error=3
+      hash,salt2=hash_password(pwd,bytes.fromhex(salt))
+      password_of_the_current_user=encryption_machine(hash)
+      # and check for authentication
+      encrypteddatabase=bytes.fromhex(encrypteddatabase)
+      if (user == userdatabase) and (encrypteddatabase == password_of_the_current_user):
+        test=True
+        error=0
+        return test,error 
   return test,error
+#endregion
 
 
 #region register
@@ -122,7 +103,6 @@ def inscription():
     save_to_database(user,pwd) 
     print("\n")
     print("Register finish")
-    print("\n")
 #endregion
 
 #region login
@@ -132,39 +112,33 @@ def connexion():
     test,error=check_password(user, pwd)
     if test==True and error==0:
         print("Good password and good user, welcome :",user)
-        print("\n")
-    elif test==False and error==1:
-        print("Error, this is the good user but not the good pwd")
-        print("\n")
-    elif test==False and error==2:
-        print("Error, this isn't the good user but the good pwd")
-        print("\n")
-    else:
-        print("Error, this isn't the good user and pwd")
-        print("\n")
-
+    if test==False and error==1:
+        print("Error, this isn't the good user or pwd")
 #endregion
 
 #region Project
 def Projet_Crypto():
     c=True
+    print("Welcome to my first Crypto-Project ! ")
     while c:
         print("\n")
         print("-------------start-------------")
-        print("Welcome to my first Crypto-Project")
         print("\n")
-        case=input("Do you want to register (Press 1) or login (Press 2) or quite (Press 3) > ")
+        case=input("Do you want to register (Press 1) or login (Press 2) or exit (Press 3) > ")
         print("\n")
         if case == "1":
             inscription()
         elif case =="2":
-            connexion()
+            if os.path.isfile(database):
+              connexion()
+            else:
+              print("Database isn't yet created, you need to register to initialize the database for the firstime")
         elif case =="3":
             c=False
             print("Au revoir !")
         else:
             print("Something, you don't press an avalaible numbers, please restart")
-        print("\n")
+       
         print("-------------end---------------")
 #endregion
 
